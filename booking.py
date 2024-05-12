7de3f27c7c435ce90dfe97163e6406562637bcc0
Samuel Livingston <roadrustersam@gmail.com>
2024-05-12 15:26:41 +0530

    picking_up

--- booking.py
+++ booking.py
@@ -6,3 +6,22 @@ from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
 
+ui, _ = loadUiType('assets/ui/booking.ui')
+
+
+class MainApp(QMainWindow, ui):
+    def __init__(self):
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+        self.tabWidget.setCurrentIndex(0)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
