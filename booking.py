adef8cf50ec54046ea111060ea23efd51782c267
Samuel Livingston <roadrustersam@gmail.com>
2024-05-14 15:20:49 +0530

    ui+code_update

--- booking.py
+++ booking.py
@@ -14,6 +14,44 @@ class MainApp(QMainWindow, ui):
         QMainWindow.__init__(self)
         self.setupUi(self)
         self.tabWidget.setCurrentIndex(0)
+        self.login_button.clicked.connect(self.login)
+        
+        self.logout_1.clicked.connect(self.logout)
+        self.logout_2.clicked.connect(self.logout)
+        self.logout_3.clicked.connect(self.logout)
+        
+        self.order_entry_1.clicked.connect(self.showOrderEntry)
+        self.order_entry_2.clicked.connect(self.showOrderEntry)
+        self.order_entry_3.clicked.connect(self.showOrderEntry)
+        
+        self.edit_orders_1.clicked.connect(self.showEditOrders)
+        self.edit_orders_2.clicked.connect(self.showEditOrders)
+        self.edit_orders_3.clicked.connect(self.showEditOrders)
+        
+        self.orders_1.clicked.connect(self.showOrders)
+        self.orders_2.clicked.connect(self.showOrders)
+        self.orders_3.clicked.connect(self.showOrders)
+        
+    def login(self):
+        usr_pwd = self.login_input.text()
+        if usr_pwd != "sagayam":
+            self.login_info.setText("Wrong Password!")
+        elif usr_pwd == "sagayam":
+            self.login_info.setText("")
+            self.login_input.setText("")
+            self.tabWidget.setCurrentIndex(1)
+        
+    def logout(self):
+        self.tabWidget.setCurrentIndex(0)
+        
+    def showOrderEntry(self):
+        self.tabWidget.setCurrentIndex(1)
+        
+    def showEditOrders(self):
+        self.tabWidget.setCurrentIndex(2)
+        
+    def showOrders(self):
+        self.tabWidget.setCurrentIndex(3)
 
 
 def main():
