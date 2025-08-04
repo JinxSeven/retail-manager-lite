be0959474f6cf50a4d43457e235816609d71ba41
Samuel Livingston <164835921+JinxSeven@users.noreply.github.com>
2025-08-04 20:49:10 +0530

    Fill Table Method
    Updated Fill-Table method to update table with initial values with no filters applied, can be recalled when clicking clear-filter button

--- src/handlers/manage_orders.py
+++ src/handlers/manage_orders.py
@@ -2,6 +2,7 @@ import datetime
 import sqlite3
 
 from PyQt5.QtCore import QDate
+from PyQt5.QtWidgets import QTableWidgetItem
 
 from src.config import DB_PATH
 from src.utils.services import Services
@@ -24,7 +25,7 @@ class ManageOrdersHandler:
         self.ui.manageOrdTbl.setColumnWidth(0, 145)
         self.ui.manageOrdTbl.setColumnWidth(1, 145)
         self.ui.manageOrdTbl.setColumnWidth(2, 145)
-        self.ui.manageOrdTbl.setColumnWidth(3, 145)
+        self.ui.manageOrdTbl.setColumnWidth(3, 115)
         self.ui.manageOrdTbl.setColumnWidth(4, 63)
         self.ui.manageOrdTbl.setColumnWidth(5, 63)
         self.ui.manageOrdTbl.setColumnWidth(6, 63)
@@ -37,8 +38,11 @@ class ManageOrdersHandler:
             cursor.execute("SELECT * FROM orders")
             results = cursor.fetchall()
             
-            if results: print(results)
-                    
+            if results:
+                for row_index, row_data in enumerate(results):
+                    self.ui.manageOrdTbl.insertRow(row_index)
+                    for colm_index, colm_data in enumerate(row_data):
+                        self.ui.manageOrdTbl.setItem(row_index, colm_index, QTableWidgetItem(str(colm_data)))
             
     #  Method to check invalid date ranges
     def invalid_date_range_check(self):
